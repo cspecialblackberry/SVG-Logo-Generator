8e8e08190bfb1b896527d1bbb69a52985ebfb5e1
Hall Family <charliesamhall@icloud.com>
2024-02-06 09:51:18 -0500

    file commit

--- develop/classes/prompts.js
+++ develop/classes/prompts.js
@@ -1,8 +1,35 @@
 const inquirer = require('inquirer')
 
 class Prompts {
-    run(){
+    run() {
         console.log('hi')
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'inputText',
+                    message: 'Enter up to three characters for your logo.'
+                },
+                {
+                    type: 'input',
+                    name: 'textColor',
+                    message: 'What color would you like your text to be?'
+                },
+                {
+                    type: 'list',
+                    name: 'shape',
+                    message: 'Which shape would you like?',
+                    choices: ['circle', 'triangle', 'square']
+                },
+                {
+                    type: 'input',
+                    name: 'shapeColor',
+                    message: 'What color would you like yout shape to be?'
+                }
+            ])
+            .then((data) => {
+                console.log(data)
+            })
     }
 }
 
