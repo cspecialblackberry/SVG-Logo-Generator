9cd0c7e4a9754179fa95d9dfa154144a3d3813d3
Hall Family <charliesamhall@icloud.com>
2024-02-07 17:40:01 -0500

    updated gitignore to include output svg file

--- develop/classes/svg.js
+++ develop/classes/svg.js
@@ -31,7 +31,7 @@ module.exports = { createSVG }
 
 // <circle cx="150" cy="110" r="80" fill="red" />
 // <rect width="150" height="150" x="75" y="35" fill="blue" />
-// <polygon points="150,10 45,180 245,180" fill="blue" /> 
+// <polygon points="150,10 45,180 245,180" fill="blue" />
 
 // <text x="150" y="125" font-size="50" text-anchor="middle" fill="white">SVG</text>
 
